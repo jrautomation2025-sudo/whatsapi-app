a3bc8dde13fc6384481d323bd76eca77988ef6cd
jrautomation2025-sudo <jrautomation2025@gmail.com>
2026-08-22 06:54:55 -0300

    Include FontAwesome in sidebar.php
    Added FontAwesome stylesheet link to sidebar.php.

--- whatsapi/sidebar.php
+++ whatsapi/sidebar.php
@@ -2,6 +2,10 @@
 // sidebar.php - Versão Consolidada (JR TECH AUTO)
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
+<head>
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
 <style>
     :root {
         --sidebar-w: 260px;

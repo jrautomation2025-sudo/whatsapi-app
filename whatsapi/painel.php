3fa235da03aee477db002dd8ed95f633fbb41432
jrautomation2025-sudo <jrautomation2025@gmail.com>
2026-08-22 06:45:03 -0300

    Add favicon link to painel.php

--- whatsapi/painel.php
+++ whatsapi/painel.php
@@ -45,6 +45,7 @@ include 'sidebar.php';
     <title>Dashboard - JR Tech Automation</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="icon" type="image/svg+xml" href="jrtec.svg">
     <style>
         :root {
             --bg-main: #0a0b0d;
